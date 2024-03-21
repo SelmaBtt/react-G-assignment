b1d19051e6aaa472a58aeb612b180127f1330811
Selma Brattli <selma@brattli.nu>
2024-03-21 14:14:41 +0100

    Some changes

--- src/components/DisplayResult.jsx
+++ src/components/DisplayResult.jsx
@@ -1,16 +1,22 @@
-
+import FetchID from "./FetchID";
 
 const DisplayResults = ({ data }) => {
+
+    const getID = (item) => {
+        console.log(item.idMeal)
+    }
+
     return(
         <>
             <ul>
                 {(data && data.length > 0) && data.map((item, idx) => ( // If data.length is higher than 0, map out the array 
-                    <li key={idx}>
+                    <li onClick={() => getID(item)} key={idx}>
                         {JSON.stringify(item.strMeal)} <br />
                         <img src={item.strMealThumb} alt="Meal image" />
                     </li>
                 ))}  
             </ul>
+            {/* <FetchID individualMealID={} /> Meal id that we get from the ID function */}
         </>
     )
 }
