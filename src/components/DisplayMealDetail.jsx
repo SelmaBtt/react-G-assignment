e91448df81ced7e6688e3f5e63d9b837fceb86c1
Selma Brattli <selma@brattli.nu>
2024-03-24 14:58:27 +0100

    meal detail css

--- src/components/DisplayMealDetail.jsx
+++ src/components/DisplayMealDetail.jsx
@@ -2,18 +2,24 @@
 //Most likely youll have to make the ingrediants into an array but do that in the FetchID 
 
 import ConvertObjToArr from "./ConvertObjToArr";
+import styled from '../stylesheets/DisplayMealDetail.module.css'
 
 const DisplayMealDetail = ({ mealDetails }) => {
     return(
-        <ul>
+        <ul className={styled.ulWrapper}>
             {(mealDetails && mealDetails.length > 0) && mealDetails.map((item, idx) => ( // If data.length is higher than 0, map out the array 
-                <li key={idx}>
-                    {item.strMeal} <br />
-                    {item.strCategory} <br />
-                    {item.strArea} <br />
-                    {item.strInstructions} <br />
-                    {item.strTags && item.strTags} <br />
+                <li className={styled.liH2Titles} key={idx}>
+                    <h1 className={styled.detailTitle}>{item.strMeal}</h1> 
+                    <h2>Chategory:</h2>
+                    {item.strCategory} 
+                    <h2>Country: </h2> 
+                    {item.strArea}
+                    <h2>Measurments: </h2>
                     <ConvertObjToArr mealObj={item} />
+                    <h2>Instructions: </h2>
+                    {item.strInstructions}
+                    <h2>Tags: </h2>
+                    {item.strTags && item.strTags}
                 </li>
             ))}  
         </ul>
